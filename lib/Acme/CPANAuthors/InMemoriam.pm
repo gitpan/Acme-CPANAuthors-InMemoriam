032453771a7423bbdddd1da6bbe43e2991622cc2
Barbie <barbie@missbarbell.co.uk>
2014-05-02 06:38:48 +0000

    Import of BARBIE/Acme-CPANAuthors-InMemoriam-1.01 from CPAN.
    gitpan-cpan-distribution: Acme-CPANAuthors-InMemoriam
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         BARBIE/Acme-CPANAuthors-InMemoriam-1.01.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/Acme/CPANAuthors/InMemoriam.pm
+++ lib/Acme/CPANAuthors/InMemoriam.pm
@@ -4,7 +4,7 @@ use warnings;
 
 {
     no strict "vars";
-    $VERSION = "1.00";
+    $VERSION = "1.01";
 }
 
 use Acme::CPANAuthors::Register (
@@ -12,6 +12,7 @@ use Acme::CPANAuthors::Register (
     # Our fallen heroes, may they never be forgotten
 
     'AMORETTE'  => 'Hojung Yoon',
+    'ANDREWF'   => 'Andrew Ford',
     'GMCCAR'    => 'Greg McCarroll',
     'IVORW'     => 'Ivor Williams',
     'NI-S'      => 'Nick Ing-Simmons',
